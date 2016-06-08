bec8a6fd35b2cb049e23bf0aa8478b2769379f2f
Erick Mattson <Erick Mattson>
2016-06-08 12:33:01 -0500

    email function

---
+++ WorkWarriors/Controllers/EmailController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WorkWarriors.Models;
+using System.Net.Http;
+using System.Web.Http;
+using System.Net.Mail;
+using SendGrid;
+
+
+
+namespace WorkWarriors.Controllers
+{
+    public class EmailController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Email
+        public ActionResult Index()
+        {
+            return View(db.Homeowners.ToList());
+        }
+
+        // GET: Email/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Homeowner homeowner = db.Homeowners.Find(id);
+            if (homeowner == null)
+            {
+                return HttpNotFound();
+            }
+            return View(homeowner);
+        }
+
+        // GET: Email/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Email/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Username,FirstName,LastName,Address,City,State,Zip,email")] Homeowner homeowner)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Homeowners.Add(homeowner);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(homeowner);
+        }
+
+        // GET: Email/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Homeowner homeowner = db.Homeowners.Find(id);
+            if (homeowner == null)
+            {
+                return HttpNotFound();
+            }
+            return View(homeowner);
+        }
+
+        // POST: Email/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Username,FirstName,LastName,Address,City,State,Zip,email")] Homeowner homeowner)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(homeowner).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(homeowner);
+        }
+
+        // GET: Email/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Homeowner homeowner = db.Homeowners.Find(id);
+            if (homeowner == null)
+            {
+                return HttpNotFound();
+            }
+            return View(homeowner);
+        }
+
+        // POST: Email/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Homeowner homeowner = db.Homeowners.Find(id);
+            db.Homeowners.Remove(homeowner);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult sendMailNow()///(string message)
+        {
+
+            db = new ApplicationDbContext();
+            var myMessage = new SendGrid.SendGridMessage();
+
+            List<String> recipients = new List<String>
+            {
+                //@"Changa Chimp <changachimp@yahoo.com>",
+                //@"Penny Wise <wisepenny79@gmail.com>",
+                //@"E. Matts <erickmattson@msn.com>",
+            };
+            
+                foreach (var i in db.Homeowners)
+                {
+                recipients.Add(i.email);
+
+                };
+
+            myMessage.AddTo(recipients);
+            myMessage.From = new MailAddress("monsymonster@msn.com", "Joe Johnson");
+            myMessage.Subject = "Sending with SendGrid is Fun";
+            //myMessage.Html = "<p>Hello World!</p>";
+            myMessage.Text = "Welcome to Work Warriors";
+            var credentials = new NetworkCredential("quikdevstudent", "Lexusi$3");
+            var transportWeb = new SendGrid.Web(credentials);
+            transportWeb.DeliverAsync(myMessage);
+
+            return RedirectToAction("About", "Home");
+
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
